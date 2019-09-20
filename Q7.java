f2c42d87e9cf53d6daebed371386663b0b311be1
28-star <54815318+28-star@users.noreply.github.com>
2019-09-20 22:01:22 +0530

    Add files via upload

---
+++ Q7.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Q7
+{public static void main(String args[])
+	{Scanner sc= new Scanner(System.in);
+		System.out.println("ENTER THE FIRST NUMBER");
+		int ia = sc.nextInt();
+
+System.out.println("ENTER THE SECOND NUMBER");
+		int ib = sc.nextInt();
+
+System.out.println("ENTER THE THIRD NUMBER");
+		int ic = sc.nextInt();
+int y;
+ia>ic ? y=ia :y=ic;
+
+System.out.println(y);
+		
+
+
+
+
+	}}
